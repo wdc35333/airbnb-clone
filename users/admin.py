5b49327d60e094db319df3bf9a2b7a70764865f7
wdc35333 <wdc35333@gmail.com>
2021-10-12 17:07:27 +0900

    에어비앤비 클론코딩 챌린지 2일차 models.py 항목 추가, admin.py 추가항 항목들 커스텀 필드셋 추가

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    pass
+    """Custom User Admin"""
+
+    # list_display = ("username", "gender", "language", "currency", "superhost")
+    # list_filter = (
+    #     "language",
+    #     "currency",
+    #     "superhost",
+    # )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
