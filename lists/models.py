b8fa86895835083b33274d381cf51ba98151a24b
wdc35333 <wdc35333@gmail.com>
2021-10-14 23:39:33 +0900

    에어비앤비 클론코딩 4일차 중간저장

--- lists/models.py
+++ lists/models.py
@@ -5,3 +5,10 @@ from core import models as core_models
 class List(core_models.TimeStampedModel):
 
     """List Model Definition"""
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    rooms = models.ManyToManyField("rooms.Room", blank=True)
+
+    def __str__(self):
+        return self.name
